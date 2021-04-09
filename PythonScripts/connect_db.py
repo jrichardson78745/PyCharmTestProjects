20761e71d3ef466002dec3ec76c08647abda296c
John Richardson <jrichardson@brainwerkz.com>
2021-04-09 12:53:41 -0500

    Update connect_db.py

--- PythonScripts/connect_db.py
+++ PythonScripts/connect_db.py
@@ -3,7 +3,7 @@ import MySQLdb
 connection = MySQLdb.connect(
                 host = 'localhost',
                 user = 'root',
-                passwd = 'JEllen69!')  # create the connection
+                passwd = '')  # create the connection
 
 cursor = connection.cursor()     # get the cursor
 
